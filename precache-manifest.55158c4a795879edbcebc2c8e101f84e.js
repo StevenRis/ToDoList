5c387ed8a39f0fa76d178c5071203eab09e646a5
trader TGN <trader@tcn.local>
2021-05-19 12:18:25 +0200

    Updates

---
+++ precache-manifest.55158c4a795879edbcebc2c8e101f84e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5a72a8bdc346d65e4d7603fced585f",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "75b432486cebdb5ac5c8",
+    "url": "/ToDoList/static/js/2.965462cf.chunk.js"
+  },
+  {
+    "revision": "a11b5a3d802d23532623",
+    "url": "/ToDoList/static/js/main.edc47641.chunk.js"
+  },
+  {
+    "revision": "2386f67870d2d75be94f",
+    "url": "/ToDoList/static/js/runtime-main.70f3f13c.js"
+  }
+]);
